5856259641ee7e9fc32c50cffa4a73a9934c376d
Erifeoluwa Jamgbadi <44726422+erifejams@users.noreply.github.com>
2022-02-13 23:11:10 +0000

    prints the name
    basic start that just prints the name of chatbot

--- Coding/Model.py
+++ Coding/Model.py
@@ -1,1 +1,18 @@
 import csv
+import json
+import numpy as np
+#HAD TO INSTALL THE LIBRARY USING "pip install nltk"
+#import nlkt
+
+#starting here
+#basic chatbot build
+class Chatbot():
+    def __init__(self, name):
+        print(name)
+        self.name = name
+
+#printing name of AI
+if __name__ == "__main__":
+    ai = Chatbot(name = "Interactive Talker")
+
+
