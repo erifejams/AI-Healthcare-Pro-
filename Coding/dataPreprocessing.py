df000c9fad58df6391bff3ddd7c2649956216ca1
Erifeoluwa Jamgbadi <44726422+erifejams@users.noreply.github.com>
2022-02-16 01:18:32 +0000

    data files
    - I'm trying to sort out each of the data file to put it in one big csv file.
    - I sorted the csv file first
    - Currently working on the txt now and need to continue on from that when I start next time

--- Coding/dataPreprocessing.py
+++ Coding/dataPreprocessing.py
@@ -1,4 +1,4 @@
-#I WANT TO PUT ALL THE DATA INTO ONE CSV FILE AS ID, QUESTION, ANSWER
+#I WANT TO PUT ALL THE DATA INTO ONE CSV FILE AS ID, QUESTION, ANSWER(response)
 #DO DATA PREPROCESSING STEPS WHICH ARE: 
     #Removing punctuations like . , ! $( ) * % @
     #Removing URLs
@@ -11,5 +11,34 @@
 import pandas as pd
 
 #reading the data
-data = pd.read_csv("Psych_data.csv")
-data.head()
+#https://www.analyticsvidhya.com/blog/2021/06/text-preprocessing-in-nlp-with-python-codes/
+data = pd.read_csv('Data/Psych_data.csv', encoding="ISO-8859-1")
+#putting only the column that I needed from Psych_data.csv
+data = data[['Answer', 'Question']]
+#this is to read the data to a csv file
+#data.to_csv('Data/trainingData.csv')
+
+
+#turn txt file to a csv file
+#then put it under a column
+#then find the word e.g human 1 and put it under a 2 separate columns e.g Question and answer
+
+humanConv = list()
+firstConv = list()
+with open('Data/human_chat.txt', encoding="utf8") as file:
+   # NormalConv = file.readlines()
+    for line in file:
+        humanConv.append(line.strip())
+        if humanConv.line.str.contains(r'\bHuman 1\b'):
+            firstConv.append(line)
+
+
+print(humanConv)
+"""
+    for i in range(len(NormalConv)):
+        if '?' in NormalConv[i]:
+            NormalConv['Question'] = NormalConv[i]
+        NormalConv['Answer'] = NormalConv[i]
+"""
+   
+#print(NormalConv)
