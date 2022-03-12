8762c5f53e894636e704065b01d6523646fae75e
Erifeoluwa Jamgbadi <44726422+erifejams@users.noreply.github.com>
2022-03-12 16:50:27 +0000

    Importing libraries for building training models
    importing libraries for building the model

--- Coding/Model.py
+++ Coding/Model.py
@@ -4,3 +4,10 @@
 #https://github.com/prasoons075/Deep-Learning-Codes/blob/master/Encoder%20Decoder%20Model/Encoder_decoder_model.ipynb
 #https://towardsdatascience.com/generative-chatbots-using-the-seq2seq-model-d411c8738ab5
 
+
+import TrainingModel as tm
+from keras.models import Model
+from keras.layers import Input, LSTM, GRU, Dense, Embedding
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
