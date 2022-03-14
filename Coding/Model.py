589c094bb1dde6ff31e30a6a837268fa7395434a
Erifeoluwa Jamgbadi <44726422+erifejams@users.noreply.github.com>
2022-03-14 01:37:01 +0000

    Code for building the model
    put the code I would use for building the model, but need to edit it and add more things before the model is actually finished

--- Coding/Model.py
+++ Coding/Model.py
@@ -4,18 +4,22 @@
 #https://github.com/prasoons075/Deep-Learning-Codes/blob/master/Encoder%20Decoder%20Model/Encoder_decoder_model.ipynb
 #https://towardsdatascience.com/generative-chatbots-using-the-seq2seq-model-d411c8738ab5
 
+
 import os
+
 #have to include this or some libraries(in tensorflow) are not found before importing tensorflow
 #apparently this problem was only found in Python 3.9.10 not python
 #MAKE SURE TO ALWAYS INCLUDE IN THE FILE **IMPORTANT
 os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin")
 
-
+import pandas as pd
 from json.encoder import py_encode_basestring
 import tensorflow as tf
 from keras.models import Model
 from keras.layers import Input, LSTM, Dense
-from keras.utils.vis_utils import plot_model
+#from keras.utils.vis_utils import plot_model
+
+trainingModel = pd.read_csv (r'Data/trainingData.csv')
 
 # configure
 num_encoder_tokens = 71
@@ -24,13 +28,15 @@ latent_dim = 256
 
 
 # Define an input sequence and process it.
-encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder_inputs = Input(shape=(trainingModel['Question'], num_encoder_tokens))
 encoder = LSTM(latent_dim, return_state=True)
 encoder_outputs, state_h, state_c = encoder(encoder_inputs)
 # We discard `encoder_outputs` and only keep the states.
 encoder_states = [state_h, state_c]
+
 # Set up the decoder, using `encoder_states` as initial state.
-decoder_inputs = Input(shape=(None, num_decoder_tokens))
+decoder_inputs = Input(shape=(trainingModel['Answer'], num_decoder_tokens))
+
 # We set up our decoder to return full output sequences,
 # and to return internal states as well. We don't use the
 # return states in the training model, but we will use them in inference.
@@ -38,8 +44,25 @@ decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
 decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
 decoder_dense = Dense(num_decoder_tokens, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
+
 # Define the model that will turn
 # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
 model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 # plot the model
 #plot_model(model, to_file='model.png', show_shapes=True)
+# define encoder inference model
+encoder_model = Model(encoder_inputs, encoder_states)
+
+
+# define decoder inference model
+decoder_state_input_h = Input(shape=(latent_dim,))
+decoder_state_input_c = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+decoder_outputs, state_h, state_c = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+decoder_states = [state_h, state_c]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
+
+# summarize model
+#pydot.call_graphviz.plot_model(encoder_model, to_file='encoder_model.png', show_shapes=True)
+#plot_model(decoder_model, to_file='decoder_model.png', show_shapes=True)
