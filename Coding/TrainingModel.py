95ab5237a7545098f79daaa2ffcb10a72a72011c
Erifeoluwa Jamgbadi <44726422+erifejams@users.noreply.github.com>
2022-03-23 01:07:38 +0000

    Seq2Seq problem
    worked on the seq2seq, couldn't make any progress

--- Coding/TrainingModel.py
+++ Coding/TrainingModel.py
@@ -1,11 +1,17 @@
 #CREATE TRAINING AND TEST LIST
 #Tokenizing the training data
 import os
-
 #have to include this or some libraries(in tensorflow) are not found before importing tensorflow
 #apparently this problem was only found in Python 3.9.10 not python
 #MAKE SURE TO ALWAYS INCLUDE IN THE FILE **IMPORTANT
 os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin")
+
+from matplotlib import pyplot as plt
+from sklearn.feature_selection import SequentialFeatureSelector
+from sklearn.preprocessing import MinMaxScaler
+from keras.layers import Dense, LSTM, Bidirectional, Embedding, Concatenate, Attention, TimeDistributed
+from keras import Input, Model
+
 
 import numpy as np
 import pandas as pd
@@ -16,7 +22,9 @@ from sklearn.model_selection import train_test_split
 #THIS IS TO READ THE TRAINING DATA
 training = pd.read_csv (r'Data/trainingData.csv')
 
+training['Answer'] = training.Answer.apply(lambda x: 'start '+ x + ' stop')
 
+#print(training['Answer'])
 # Convert into list of sentence we need list to pass in tokenizer
 question_list = training.Question.to_list()
 answer_list = training.Answer.to_list()
@@ -46,7 +54,7 @@ question_word_index = question_tokenizer.word_index
 # For zero padding we have to add +1 in size
 QUESTION_VOCAB_SIZE = len(question_tokenizer.word_counts)+1
 
-# Marathi Word --> index dict
+# answer Word --> index dict
 answer_word_index= answer_tokenizer.word_index
 #answer vocab size for decoder output
 ANSWER_VOCAB_SIZE = len(answer_tokenizer.word_counts)+1
@@ -87,4 +95,55 @@ training = np.array(training)
 train_x = list(training[:, 1])
 train_y = list(training[:, 2])
 #print(training)
+
+"""
+"""
+# Encoder input
+encoder_inputs = Input(shape=(max_question_len)) 
+
+# Embedding layer- i am using 1024 output-dim for embedding you can try diff values 100,256,512,1000
+enc_emb = Embedding(QUESTION_VOCAB_SIZE, 1024)(encoder_inputs)
+
+# Bidirectional lstm layer
+enc_lstm1 = Bidirectional(LSTM(512,return_sequences=True,return_state=True))
+##encoder_outputs1, forw_state_h, forw_state_c, back_state_h, back_state_c = enc_lstm1(enc_emb)
+encoder_output, forw_state_h, forw_state_c, back_state_h, back_state_c = enc_lstm1(enc_emb)
+
+
+# get Context vector
+encoder_states =[forw_state_h, forw_state_c, back_state_h, back_state_c]
+
+
+#  decoder input
+decoder_inputs = Input(shape=(max_answer_len)) 
+
+# decoder embedding with same number as encoder embedding
+dec_emb = Embedding(ANSWER_VOCAB_SIZE, 1024)(decoder_inputs) 
+# apply this way because we need embedding layer for prediction 
+
+# In encoder we used Bidirectional so it's having two LSTM's so we have to take double units(256*2=512) for single decoder lstm
+# LSTM using encoder's final states as initial state
+decoder_lstm = LSTM(512, return_sequences=True, return_state=True) 
+decoder_outputs, _, _ = decoder_lstm(dec_emb, initial_state = encoder_states)
+
+# Using Attention Layer
+#attention_layer = AttentionLayer()
+attention_layer = Attention()
+attention_result, attention_weights = attention_layer([encoder_output, decoder_outputs])
+
+# Concat attention output and decoder LSTM output 
+decoder_concat_input = Concatenate(axis=-1, name='concat_layer')([decoder_outputs, attention_result])
+
+# Dense layer with softmax
+decoder_dense = Dense(ANSWER_VOCAB_SIZE, activation='softmax')
+decoder_outputs = decoder_dense(decoder_concat_input)
+
+attention_result, attention_weights = attention_layer([encoder_output, decoder_outputs])
+dense1 = Dense(20, activation="relu")(attention_result)
+output = Dense(1, activation="sigmoid")
+
+# Define the model
+model = Model(inputs = [encoder_inputs, decoder_inputs], outputs = decoder_outputs)
+
+model.summary()
 """
--- Coding/seqexampleYouTube.py
+++ Coding/seqexampleYouTube.py
@@ -1,4 +1,6 @@
 import os
+import numpy as np
+import pandas as pd
 
 #have to include this or some libraries(in tensorflow) are not found before importing tensorflow
 #apparently this problem was only found in Python 3.9.10 not python
@@ -10,29 +12,44 @@ import tensorflow as tf
 from tensorflow import keras as ks
 from keras.utils.vis_utils import plot_model
 import TrainingModel as tm
-from keras.layers import Dense, LSTM, Bidirectional, Embedding, Concatenate
+from keras.layers import Dense, LSTM, Bidirectional, Embedding, Concatenate, GlobalAveragePooling1D
 from keras import Input, Model, layers
 from keras.layers.attention import Attention
 import TrainingModel as tm
 from keras.models import load_model
 
-
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-
+"""
 model = ks.Sequential()
-model.add(layers.Embedding(input_length = 3120, input_dim=10000, output_dim=3120))    
+model.add(layers.Embedding(input_length = 3120, input_dim=10000, output_dim=10000))    
 model.add(layers.LSTM(64, return_sequences=True))
 model.add(layers.LSTM(64))
-model.add(layers.Dense(585, activation="softmax"))
+model.add(layers.Dense(587, activation="softmax"))
 model.compile(optimizer=ks.optimizers.SGD(learning_rate=(0.55)), loss=ks.losses.CategoricalCrossentropy(), metrics=['accuracy'])
 model.summary()
+"""
 
-#TF_CUDNN_RESET_RND_GEN_STATE=1
-model.fit(tm.X_train, tm.y_train, batch_size=5, epochs=25)
+vocab_size = 1000
+embedding_dim = 16
+max_len = 20
+model = ks.Sequential()
+model.add(Embedding(vocab_size, embedding_dim, input_length=3120))
+model.add(GlobalAveragePooling1D())
+model.add(Dense(16, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(3000, activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy', 
+              optimizer='adam', metrics=['accuracy'])
+
+model.summary()
 
 
+epochs = 20
+#history = model.fit(padded_sequences, np.array(training_labels), epochs=epochs)
+model.fit(tm.question_padded,np.array(tm.answer_padded),  epochs=epochs)
 #model.save("rnn")
 
 
